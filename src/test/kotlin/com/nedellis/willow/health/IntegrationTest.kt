06ac2d79a3ee9f9584d61e2dfeb204409faa8a82
Ned Ellis <epe2@illinois.edu>
2020-10-07 16:27:33 -0400

    Added failure mode for RPC layer

--- src/test/kotlin/com/nedellis/willow/health/IntegrationTest.kt
+++ src/test/kotlin/com/nedellis/willow/health/IntegrationTest.kt
@@ -6,16 +6,32 @@ import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import java.net.URI
 
-private class LocalHealthRpc() : HealthRpc {
-    val incomingMessages = mutableMapOf<URI, MutableList<Table>>().withDefault { mutableListOf() }
+
+sealed class NetworkStatus
+object Healthy : NetworkStatus()
+object Fail : NetworkStatus()
+data class PartialFail(val failedMembers: Set<URI>) : NetworkStatus()
+
+private class LocalHealthRpc(var networkStatus: NetworkStatus = Healthy) : HealthRpc {
+    val incomingMessages = mutableMapOf<URI, List<Table>>().withDefault { listOf() }
     // TODO: Find a way to make outgoingMessages
 
     lateinit var members: Map<URI, HealthService>
 
     override suspend fun update(dest: URI, request: Table): Empty {
-        incomingMessages.getValue(dest).add(request)
-
-        members[dest]!!.update(request)
+        when (networkStatus) {
+            is Healthy -> {
+                incomingMessages[dest] = incomingMessages.getValue(dest).plus(listOf(request))
+                members[dest]!!.update(request)
+            }
+            is Fail -> Unit
+            is PartialFail -> {
+                if (!(networkStatus as PartialFail).failedMembers.contains(dest)) {
+                    incomingMessages[dest] = incomingMessages.getValue(dest).plus(listOf(request))
+                    members[dest]!!.update(request)
+                }
+            }
+        }
         return Empty.getDefaultInstance()
     }
 }
@@ -40,11 +56,10 @@ class IntegrationTest : StringSpec({
         return Pair(services, healthRpc)
     }
 
-    "Health System with One Node" {
+    "Connected Health System with One Node" {
         val (services, healthRpc) = buildServices(1)
 
         for (cycleCount in 0..100) {
-
             /**
              * Before Gossip: Health should be the number of current clock cycles
              * After Gossip: Health should be the number of current clock cycles + 1
@@ -55,6 +70,61 @@ class IntegrationTest : StringSpec({
                 it.incrementAndGossip()
                 it.table.entriesMap shouldBe mapOf(it.config.address.toString() to cycleCount + 1L)
                 healthRpc.incomingMessages.getValue(it.config.address).size shouldBe 0
+            }
+        }
+    }
+
+    "Connected Health System with Two Nodes" {
+        val (services, healthRpc) = buildServices(2)
+
+        for (cycleCount in 0..100) {
+            // Before IncrementAndGossip()
+            services.forEach {
+                it.table.entriesMap shouldBe services.map { it.config.address.toString() to cycleCount }.toMap()
+            }
+
+            services.forEach { it.incrementAndGossip() }
+
+            // After IncrementAndGossip()
+            services.forEach {
+                it.table.entriesMap shouldBe services.map { it.config.address.toString() to cycleCount + 1L }.toMap()
+                healthRpc.incomingMessages.getValue(it.config.address).size shouldBe cycleCount + 1
+            }
+        }
+    }
+
+    "Eventually Failed Health System with Two Nodes" {
+        val (services, healthRpc) = buildServices(2)
+
+        for (cycleCount in 0 until 10) {
+            // Before IncrementAndGossip()
+            services.forEach {
+                it.table.entriesMap shouldBe services.map { it.config.address.toString() to cycleCount }.toMap()
+            }
+
+            services.forEach { it.incrementAndGossip() }
+
+            // After IncrementAndGossip()
+            services.forEach {
+                it.table.entriesMap shouldBe services.map { it.config.address.toString() to cycleCount + 1L }.toMap()
+                healthRpc.incomingMessages.getValue(it.config.address).size shouldBe cycleCount + 1
+            }
+        }
+
+        healthRpc.networkStatus = Fail
+
+        for (cycleCount in 10 until 100) {
+            services.forEach { it.incrementAndGossip() }
+
+            // After IncrementAndGossip()
+            services.forEach {
+                val expectedTable = services.map { svc ->
+                    if (svc.config.address == it.config.address) svc.config.address.toString() to cycleCount + 1L
+                    else svc.config.address.toString() to 10L
+                }.toMap()
+
+                it.table.entriesMap shouldBe expectedTable
+                healthRpc.incomingMessages.getValue(it.config.address).size shouldBe 10L
             }
         }
     }
