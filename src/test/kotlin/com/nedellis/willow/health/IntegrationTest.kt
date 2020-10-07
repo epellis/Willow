10f88f35fd8f7d829985b0cc601b4da190d9e1f4
Ned Ellis <epe2@illinois.edu>
2020-10-07 15:36:04 -0400

    More invariants

--- src/test/kotlin/com/nedellis/willow/health/IntegrationTest.kt
+++ src/test/kotlin/com/nedellis/willow/health/IntegrationTest.kt
@@ -7,9 +7,14 @@ import io.kotest.matchers.shouldBe
 import java.net.URI
 
 private class LocalHealthRpc() : HealthRpc {
+    val incomingMessages = mutableMapOf<URI, MutableList<Table>>().withDefault { mutableListOf() }
+    // TODO: Find a way to make outgoingMessages
+
     lateinit var members: Map<URI, HealthService>
 
     override suspend fun update(dest: URI, request: Table): Empty {
+        incomingMessages.getValue(dest).add(request)
+
         members[dest]!!.update(request)
         return Empty.getDefaultInstance()
     }
@@ -19,7 +24,7 @@ class IntegrationTest : StringSpec({
     /**
      * Build initialized health services, connected to each other by [LocalHealthRpc]
      */
-    fun buildServices(count: Int): List<HealthService> {
+    fun buildServices(count: Int): Pair<List<HealthService>, LocalHealthRpc> {
         val healthRpc = LocalHealthRpc()
 
         val addreses = (8000 until 8000 + count).map { port ->
@@ -32,17 +37,24 @@ class IntegrationTest : StringSpec({
 
         healthRpc.members = services.map { it.config.address to it }.toMap()
 
-        return services
+        return Pair(services, healthRpc)
     }
 
     "Health System with One Node" {
-        val services = buildServices(1)
+        val (services, healthRpc) = buildServices(1)
 
         for (cycleCount in 0..100) {
+
+            /**
+             * Before Gossip: Health should be the number of current clock cycles
+             * After Gossip: Health should be the number of current clock cycles + 1
+             * Always (for one node): No messages should be sent to it
+             */
             services.forEach {
                 it.table.entriesMap shouldBe mapOf(it.config.address.toString() to cycleCount)
                 it.incrementAndGossip()
                 it.table.entriesMap shouldBe mapOf(it.config.address.toString() to cycleCount + 1L)
+                healthRpc.incomingMessages.getValue(it.config.address).size shouldBe 0
             }
         }
     }
